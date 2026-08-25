0167f760ca42e725853f1cd8b8d7be30026bae5c
PRATHAMKUMAR . <prathamkumar@nashtechlabs.com>
2026-08-25 11:27:20 +0530

    Add FluentValidation pipeline behavior

---
+++ TaskFlow.Application/Tasks/Commands/CreateTaskValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace TaskFlow.Application.Tasks.Commands
+{
+    public class CreateTaskValidator : AbstractValidator<CreateTaskCommand>
+    {
+        public CreateTaskValidator() 
+        {
+            RuleFor(x => x.ProjectId).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty().MaximumLength(200);
+        }
+    }
+}
